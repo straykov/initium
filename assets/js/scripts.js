8d1af57a188c5604f86cd2b96684da30ddb8c764
straykov <ilya@straykov.ru>
2015-06-18 16:29:56 +0300

    Небольшая уборка, Джейквери поновее

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,1 @@
-$("body").click(function(){$("#initium").html(" Начинай уже")});
+$(document).ready(function(){$(document).on("mouseover mouseout","a",function(a){var b=$(this).attr("href");b&&"#"!=b&&$("a").filter('[href="'+$(this).attr("href")+'"]').toggleClass("hover","mouseover"==a.type)})});
