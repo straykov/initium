05b85485eeefdac6294f81ddcf8c92f8a73afeb6
Oleh Aloshkin <olegaleshkin@gmail.com>
2016-10-12 02:46:27 +0300

    Added gulp-pug

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,21 @@ var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     portfinder = require('portfinder'),
     postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
     nested = require("postcss-nested"),
-    debug = require('gulp-debug'),
     cssnext = require("postcss-cssnext"),
     vars = require('postcss-simple-vars'),
     imprt = require('postcss-import'),
-    cache = require('gulp-cached'),
-    remember = require('gulp-remember'),
     nano = require('gulp-cssnano'),
     browserSync = require("browser-sync"),
-    reload = browserSync.reload,
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
-    eslint = require('gulp-eslint'),
-    include = require("gulp-html-tag-include"),
+    pug = require('gulp-pug'),
+    inline  = require('postcss-inline-svg'),
+    cache = require('gulp-cached'),
+    remember = require('gulp-remember'),
     del = require('del'),
-    path = require('path');
+    path = require('path'),
+    reload = browserSync.reload;
 
 // Ресурсы проекта
 var paths = {
@@ -36,17 +34,17 @@ var paths = {
 
 // Одноразовая сборка проекта
 gulp.task('default', function() {
-  gulp.start('include', 'styles', 'scripts');
+  gulp.start('pug', 'styles', 'scripts');
 });
 
 // Запуск живой сборки
 gulp.task('live', function() {
-  gulp.start('server', 'include', 'styles', 'scripts', 'watch');
+  gulp.start('server', 'pug', 'styles', 'scripts', 'watch');
 });
 
 // Туннель
 gulp.task('external-world', function() {
-  gulp.start('web-server', 'include', 'styles', 'scripts', 'watch');
+  gulp.start('web-server', 'pug', 'styles', 'scripts', 'watch');
 });
 
 // Федеральная служба по контролю за оборотом файлов
@@ -61,7 +59,7 @@ gulp.task('watch', function() {
   };
   var templates = {
     path: paths.templates,
-    task: gulp.watch(paths.templates + '**/*.html', ['include', 'html'])
+    task: gulp.watch(paths.templates + '**/*.pug', ['pug', 'html'])
   };
 
   var watch = [styles, scripts, templates];
@@ -82,7 +80,7 @@ gulp.task('watch', function() {
     var folder = (e.path).split(path.sep);
     if (folder[folder.length - 2] === 'templates') {
       var filePathFromSrc = path.relative(path.resolve(paths.templates), e.path);
-      var destFilePath = path.resolve('./', filePathFromSrc);
+      var destFilePath = path.resolve('./', path.parse(filePathFromSrc).name) + '.html';
       del.sync(destFilePath);
     }
   }
@@ -94,13 +92,13 @@ gulp.task('watch', function() {
 });
 
 // Шаблонизация
-gulp.task('include', function() {
-  return gulp.src(paths.templates + '*.html')
+gulp.task('pug', function() {
+  return gulp.src(paths.templates + '*.pug')
     .pipe(cache(paths.templates))
     .pipe(remember(paths.templates))
     .pipe(plumber({errorHandler: onError}))
-    .pipe(include())
-    .pipe(gulp.dest(paths.html))
+    .pipe(pug({pretty: true}))
+    .pipe(gulp.dest(paths.html));
 });
 
 // Компиляция стилей, добавление префиксов
@@ -109,7 +107,8 @@ gulp.task('styles', function () {
     imprt(),
     cssnext(),
     vars(),
-    nested()
+    nested(),
+    inline()
   ];
   return gulp.src(paths.styles + 'layout.pcss')
     .pipe(plumber({errorHandler: onError}))
@@ -126,8 +125,6 @@ gulp.task('scripts', function() {
     .pipe(cache(paths.scripts))
     .pipe(remember(paths.scripts))
     .pipe(plumber({errorHandler: onError}))
-    .pipe(eslint())
-    .pipe(eslint.format())
     .pipe(concat('scripts.js'))
     .pipe(uglify())
     .pipe(gulp.dest(paths.js))
