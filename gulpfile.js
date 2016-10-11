4a518be3c6c984c2effd21fe65bea3f80daba543
Oleh Aloshkin <olegaleshkin@gmail.com>
2016-10-12 00:20:15 +0300

    Caching of files

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,21 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     nested = require("postcss-nested"),
+    debug = require('gulp-debug'),
     cssnext = require("postcss-cssnext"),
     vars = require('postcss-simple-vars'),
     imprt = require('postcss-import'),
+    cache = require('gulp-cached'),
+    remember = require('gulp-remember'),
     nano = require('gulp-cssnano'),
     browserSync = require("browser-sync"),
     reload = browserSync.reload,
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     eslint = require('gulp-eslint'),
-    include = require("gulp-html-tag-include");
+    include = require("gulp-html-tag-include"),
+    del = require('del'),
+    path = require('path');
 
 // Ресурсы проекта
 var paths = {
@@ -46,18 +51,56 @@ gulp.task('external-world', function() {
 
 // Федеральная служба по контролю за оборотом файлов
 gulp.task('watch', function() {
-  gulp.watch(paths.styles + '**/*.css', ['styles']);
-  gulp.watch(paths.scripts + '*.js', ['scripts']);
-  gulp.watch(paths.templates + '**/*.html', ['include', 'html']);
-  gulp.watch(paths.templates + 'blocks/*.html', ['include', 'html']);
+  var styles = {
+    path: paths.styles,
+    task: gulp.watch(paths.styles + '**/*.css', ['styles'])
+  };
+  var scripts = {
+    path: paths.scripts,
+    task: gulp.watch(paths.scripts + '*.js', ['scripts'])
+  };
+  var templates = {
+    path: paths.templates,
+    task: gulp.watch(paths.templates + '**/*.html', ['include', 'html'])
+  };
+
+  var watch = [styles, scripts, templates];
+
+  for (var tasks = 0; tasks < watch.length; tasks++) {
+    var currPath = watch[tasks].path;
+    watch[tasks].task.on('change', function(event) {
+      if (event.type === 'deleted') {
+        clearCache(event, currPath);
+        if (currPath === paths.templates) {
+          deleteTemplateFile(event);
+        }
+      }
+    });
+  }
+
+  function deleteTemplateFile (e) {
+    var folder = (e.path).split('/');
+    if (folder[folder.length - 2] === 'templates') {
+      var filePathFromSrc = path.relative(path.resolve(paths.templates), e.path);
+      var destFilePath = path.resolve('./', filePathFromSrc);
+      del.sync(destFilePath);
+    }
+  }
+
+  function clearCache (e, currPath) {
+    delete cache.caches[currPath][e.path];
+    remember.forget(currPath, e.path);
+  }
 });
 
 // Шаблонизация
 gulp.task('include', function() {
   return gulp.src(paths.templates + '*.html')
-  .pipe(plumber({errorHandler: onError}))
-  .pipe(include())
-  .pipe(gulp.dest(paths.html));
+    .pipe(cache(paths.templates))
+    .pipe(remember(paths.templates))
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(include())
+    .pipe(gulp.dest(paths.html))
 });
 
 // Компиляция стилей, добавление префиксов
@@ -66,28 +109,29 @@ gulp.task('styles', function () {
     imprt(),
     cssnext(),
     vars(),
-    nested(),
-    autoprefixer(),
+    nested()
   ];
   return gulp.src(paths.styles + 'layout.css')
-  .pipe(plumber({errorHandler: onError}))
-  .pipe(postcss(processors))
-  .pipe(rename('style.css'))
-  .pipe(nano({convertValues: {length: false}}))
-  .pipe(gulp.dest(paths.css))
-  .pipe(reload({stream: true}));
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(postcss(processors))
+    .pipe(rename('style.css'))
+    .pipe(nano({convertValues: {length: false}}))
+    .pipe(gulp.dest(paths.css))
+    .pipe(reload({stream: true}));
 });
 
 // Сборка и минификация скриптов
 gulp.task('scripts', function() {
   return gulp.src(paths.scripts + '*.js')
-  .pipe(plumber({errorHandler: onError}))
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(concat('scripts.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest(paths.js))
-  .pipe(reload({stream: true}));
+    .pipe(cache(paths.scripts))
+    .pipe(remember(paths.scripts))
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.js))
+    .pipe(reload({stream: true}));
 });
 
 // Запуск локального сервера
