f0f347991c7d0f7a3d98a32416de11549187eafa
z <z>
2017-02-08 18:05:50 +0300

    Build project

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,1 +1,1 @@
-"use strict";var example=function(e){return console.log("Hello, "+e)};example("World!");
+"use strict";var example=function(e){return console.log("Hello, "+e)};example("World!"),$(document).ready(function(){$(document).on("mouseover mouseout","a",function(e){var o=$(this).attr("href");o&&"#"!=o&&$("a").filter('[href="'+$(this).attr("href")+'"]').toggleClass("hover","mouseover"==e.type)})});
